b0168ceff70155dabfbe32b70331abbb0d1f8ae3
Matheus Massari <massari.matheus@gmail.com>
2022-09-02 17:05:51 -0300

    Cardapio 2 set

--- pages/bebidas.js
+++ pages/bebidas.js
@@ -30,59 +30,6 @@ const Bebidas = () => {
                         </Box>
                     );
                 })}
-                {/* <Accordion allowToggle>
-                    {bebidas.map((entrada, index) => {
-                        const { titulo, valor, desc, categoria, img } = entrada;
-                        if (categoria === "bebidas") {
-                            return (
-                                <AccordionItem
-                                    key={index}
-                                    borderColor="rgba(182, 144, 119, 0.2)"
-                                    focusBorderColor="pink.400"
-                                >
-                                    <AccordionButton>
-                                        <Box flex="1" textAlign="left">
-                                            <Text
-                                                fontFamily={
-                                                    "Poppins, sans-serif"
-                                                }
-                                                color="#8E3200"
-                                            >
-                                                {titulo}
-                                            </Text>
-                                        </Box>
-                                        <AccordionIcon color="#d9ac8d" />
-                                        <Text
-                                            fontFamily={"Poppins, sans-serif"}
-                                            color="#8E3200"
-                                        >
-                                            {valor}
-                                        </Text>
-                                    </AccordionButton>
-
-                                    <AccordionPanel pb={4}>
-                                        <Center my="1rem">
-                                            <Image
-                                                src={img}
-                                                alt="bolinho"
-                                                boxSize="300px"
-                                                objectFit="cover"
-                                                borderRadius="8px"
-                                                border="2px solid #8E3200"
-                                            />
-                                        </Center>
-                                        <Text
-                                            color="black"
-                                            fontFamily={"Poppins, sans-serif"}
-                                        >
-                                            {desc}
-                                        </Text>
-                                    </AccordionPanel>
-                                </AccordionItem>
-                            );
-                        }
-                    })}
-                </Accordion> */}
                 <Heading
                     fontFamily={"Allura, sans-serif"}
                     color="#8E3200"
@@ -109,58 +56,6 @@ const Bebidas = () => {
                         );
                     }
                 })}
-                {/* <Accordion allowToggle>
-                    {bebidas.map((entrada, index) => {
-                        const { titulo, valor, desc, categoria, img } = entrada;
-                        console.log(img);
-                        if (categoria === "drinks") {
-                            return (
-                                <AccordionItem key={index}>
-                                    <h2>
-                                        <AccordionButton>
-                                            <Box flex="1" textAlign="left">
-                                                <Text
-                                                    fontFamily={
-                                                        "Poppins, sans-serif"
-                                                    }
-                                                    color="#8E3200"
-                                                >
-                                                    {titulo}
-                                                </Text>
-                                            </Box>
-                                            <AccordionIcon color="#d9ac8d" />
-                                            <Text
-                                                fontFamily={
-                                                    "Poppins, sans-serif"
-                                                }
-                                                color="#8E3200"
-                                            >
-                                                {valor}
-                                            </Text>
-                                        </AccordionButton>
-                                    </h2>
-                                    <AccordionPanel pb={4}>
-                                        <Center my="1rem">
-                                            <Image
-                                                src={img}
-                                                alt="bolinho"
-                                                boxSize="300px"
-                                                objectFit="cover"
-                                                borderRadius="8px"
-                                            />
-                                        </Center>
-                                        <Text
-                                            color="black"
-                                            fontFamily={"Poppins, sans-serif"}
-                                        >
-                                            {desc}
-                                        </Text>
-                                    </AccordionPanel>
-                                </AccordionItem>
-                            );
-                        }
-                    })}
-                </Accordion> */}
             </Box>
             <Footer></Footer>
         </>
